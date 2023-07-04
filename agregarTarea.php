405c2ee75213e0805a015087d78e7abe6e988f00
Reyes Angel Castellanos Martinez <70250315+kings8963@users.noreply.github.com>
2023-07-04 15:41:07 -0600

    Add files via upload

---
+++ agregarTarea.php
@@ -0,0 +1,43 @@
+<?php
+    
+    try{
+        $conn = new PDO('mysql:host=localhost;port=3307;dbname=php_todo_list','root','');
+        //echo "Conexion establecida...";
+    }catch(PDOException $e){
+        echo"Error de conexion:";
+    }
+
+    if(isset($_POST['id'])){
+        $id=$_POST['id'];
+        $completado=(isset($_POST['completado']))?1:0;
+        
+        $sql="UPDATE tareas SET completado=? WHERE id=?";
+        $sentencia=$conn->prepare($sql);
+        $sentencia->execute([$completado,$id]);
+
+    }
+
+    if(isset($_POST['agregar_tarea'])){
+        $tarea=($_POST['tarea']);
+        //print_r($tarea);
+        $sql='INSERT INTO tareas (tarea) VALUE(?)';
+        $sentencia = $conn->prepare($sql);
+        $sentencia->execute([$tarea]);
+    }
+
+    if(isset($_GET['id'])){
+        $id=$_GET['id'];
+        $sql="DELETE FROM tareas WHERE id=?";
+        $sentencia=$conn->prepare($sql);
+        $sentencia->execute([$id]);
+    }
+
+
+    $sql="SELECT * FROM tareas";
+    $registros=$conn->query($sql);
+
+   // print_r($registros);
+
+
+
+?>
